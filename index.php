9ee95a0b4e7c07ea3722c79eb0a70a1578be5278
IsraelBurial <60294088+IsraelBurial@users.noreply.github.com>
2021-12-23 06:10:23 -0500

    changed index

--- index.php
+++ index.php
@@ -1,61 +1,69 @@
+   
+<?php 
+
+    $title = 'Index';
+    require_once 'includes/header.php'
     
-<?php
-    $title = 'Index'; 
-
-    require_once 'includes/header.php'; 
-    require_once 'db/conn.php'; 
-
-    $results = $crud->getFans();
-
 ?>
+
+
+<div class="img-container">
+    <img src="uploads/artists/Soul Music.png" class="d-block w-100 h-100" alt="Soul Artists">
+    <div class="text-block">
+        <h2>OUR FAVS</h2>    
+    </div>
+</div>
+
+<div class="row row-cols-1 row-cols-md-3 g-4 back" style="margin:1%;">
+  <div class="col">
+    <div class="card h-100">
+      <img src="uploads/artists/Luther Vandross.jpg" class="card-img-top" alt="Luther Vandross">
+      <div class="card-body text-center">
+        <h5 class="card-title">Luther Vandross</h5>
+        <p class="card-text">Luther Vandross was a singer and songwriter whose smooth style and voice lent themselves to romantic ballads.(biography.com)</p>
+        <a href="lutherinfo.php"  class="btn btn-primary">Read More...</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="uploads/artists/Patti LaBelle.jpg" class="card-img-top" alt="PattiLaBelle">
+      <div class="card-body">
+        <h5 class="card-title text-center">Patti LaBelle</h5>
+        <p class="card-text text-center">Patti LaBelle is an African American singer and actress who has been called the Godmother of Soul. She is best known for hits like "If Only You Knew," "New Attitude" and "Stir It Up."(biography.com)</p>
+        <a href="pattiinfo.php"  class="btn btn-primary">Read More...</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="uploads/artists/Lenny Williams.jpg" class="card-img-top" alt="Lenny Williams">
+      <div class="card-body text-center">
+        <h5 class="card-title">Lenny Williams</h5>
+        <p class="card-text text-center">Soul singer Lenny Williams is an influential rhythm and blues (R&B) artist who is best known for his time as the lead singer of funk band Tower of Power in the mid-1970s.(encyclopediaofarkansas.com)</p>
+        <a href="lennyinfo.php"  class="btn btn-primary">Read More...</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="uploads/artists/Anita Baker.jpg" class="card-img-top" alt="Anita Baker">
+      <div class="card-body text-center">
+        <h5 class="card-title">Anita Baker</h5>
+        <p class="card-text text-center">With her classy, refined brand of romantic soul, Anita Baker was one of the definitive quiet storm singers of the '80s.</p>
+        <a href="anitainfo.php" class="btn btn-primary">Read More...</a>
+      </div>
+    </div>
+  </div>
     
-    <h1 class="text-center">Registration for Soul Fans </h1>
-
-    <form method="post" action="success.php" enctype="multipart/form-data">
-        <div class="form-group">
-            <label for="firstname">First Name</label>
-            <input required type="text" class="form-control" id="firstname" name="firstname">
-        </div>
-        <div class="form-group">
-            <label for="lastname">Last Name</label>
-            <input required type="text" class="form-control" id="lastname" name="lastname">
-        </div>
-        <div class="form-group">
-            <label for="dob">Date Of Birth</label>
-            <input type="text" class="form-control" id="dob" name="dob">
-        </div>
-        <div class="form-group">
-            <label for="gender">Gender</label>
-            <select class="form-control" id="gender" name="gender">
-                <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) {?>
-                   <option value="<?php echo $r['gender_id'] ?>"><?php echo $r['name']; ?></option>
-                <?php }?>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="email">Email address</label>
-            <input required type="email" class="form-control" id="email"  name="email" aria-describedby="emailHelp" >
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-groupform-group">
-            <label for="phone">Contact Number</label>
-            <input type="text" class="form-control" id="phone" name="phone" aria-describedby="phoneHelp" >
-            <small id="phoneHelp" class="form-text text-muted">We'll never share your number with anyone else.</small>
-        </div>
-        <br/>
-        <div class="custom-file">
-            <input type="file" accept="image/*" class="custom-file-input" id="avatar" name="avatar" >
-            <label class="custom-file-label" for="avatar">Choose File</label>
-            <small id="avatar" class="form-text text-muted">File Upload is Optional</small>
-
-        </div>
-        
-        
-        <button type="submit" name="submit" class="btn btn-primary btn-block">Submit</button>
-    </form>
+  
 <br>
 <br>
 <br>
 <br>
 <br>
-<?php require_once 'includes/footer.php'; ?>
+<br>
+<br>
+<br>
+<br>
+<?php require_once 'includes/footer.php'?>
